b5b5fccee9fcfa42397e0e04bbd7fdc28b51a555
Florent Solt <florent@solt.biz>
2014-10-15 14:57:32 +0200

    Missing helper

--- app/controllers/team_controller.rb
+++ app/controllers/team_controller.rb
@@ -1,14 +1,16 @@
 class TeamController < ApplicationController
 
-  def index
-  	group = Setting.plugin_redmine_team_page['group'].to_i
-  	group = Group.find(group) rescue nil
-  	if group.nil?
-  		@users = User.all - [User.anonymous]
-  	else
-  		@users = User.in_group(group)
-  	end
-  	@users.sort! {|a,b| a.name <=> b.name }
-  end
+	helper :custom_fields
+
+	def index
+		group = Setting.plugin_redmine_team_page['group'].to_i
+		group = Group.find(group) rescue nil
+		if group.nil?
+			@users = User.all - [User.anonymous]
+		else
+			@users = User.in_group(group)
+		end
+		@users.sort! {|a,b| a.name <=> b.name }
+	end
 
 end
